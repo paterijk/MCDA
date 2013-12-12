f1d30784d43a4d755a5293bb32286b238a045876
Patrick Meyer <patrick.meyer@telecom-bretagne.eu>
2013-12-12 13:12:03 +0100

    Added a test to stop the function when the ranges of the performanceTable are not compatible with those of the valueFunctions.

--- R/applyPiecewiseLinearValueFunctionsOnPerformanceTable.R
+++ R/applyPiecewiseLinearValueFunctionsOnPerformanceTable.R
@@ -38,98 +38,114 @@
 ##############################################################################
 
 applyPiecewiseLinearValueFunctionsOnPerformanceTable <- function(valueFunctions, performanceTable, criteriaMinMax, alternativesIDs = NULL, criteriaIDs = NULL){
-	
-	## check the input data
   
-	if (!((is.matrix(performanceTable) || (is.data.frame(performanceTable))))) 
-        	stop("wrong performanceTable, should be a matrix or a data frame")
+  ## check the input data
+  
+  if (!((is.matrix(performanceTable) || (is.data.frame(performanceTable))))) 
+    stop("wrong performanceTable, should be a matrix or a data frame")
   
   if (!(is.null(alternativesIDs) || is.vector(alternativesIDs)))
-        	stop("alternatives IDs should be in a vector")
-        	
+    stop("alternatives IDs should be in a vector")
+  
   if (!(is.null(criteriaIDs) || is.vector(criteriaIDs)))
-        	stop("criteria IDs should be in a vector")
-	
-	## filter the data according to the given criteria and alternatives
-	
-	if (!is.null(criteriaIDs)){
-	  valueFunctions <- valueFunctions[criteriaIDs]
-	  performanceTable <- performanceTable[,criteriaIDs]
-	}
+    stop("criteria IDs should be in a vector")
   
-	if (!is.null(alternativesIDs)){
-	  performanceTable <- performanceTable[alternativesIDs,]
-	} 
+  ## filter the data according to the given criteria and alternatives
   
-  # print(valueFunctions)
+  if (!is.null(criteriaIDs)){
+    valueFunctions <- valueFunctions[criteriaIDs]
+    performanceTable <- performanceTable[,criteriaIDs]
+  }
   
-	# -------------------------------------------------------
+  if (!is.null(alternativesIDs)){
+    performanceTable <- performanceTable[alternativesIDs,]
+  } 
   
-	numCrit <- dim(performanceTable)[2]
-  
-  # print(numCrit)
-  
-	numAlt <- dim(performanceTable)[1]
-
-  # print(numAlt)
   # -------------------------------------------------------
-	
+  
+  numCrit <- dim(performanceTable)[2]
+  
+  numAlt <- dim(performanceTable)[1]
+  
+  # -------------------------------------------------------
+  
+  ## check if performanceTable values lie in the ranges of the valueFunctions values
+  
+  test <- TRUE
+  i<-1
+  
+  while(test && (i<=numCrit)){
+    mini <- min(performanceTable[,i])
+    maxi <- max(performanceTable[,i])
+    
+    vFmini <- min(valueFunctions[[i]]["x",])
+    vFmaxi <- max(valueFunctions[[i]]["x",])
+    
+    if (!((mini >= vFmini) && (maxi <= vFmaxi)))
+      test<-FALSE
+    
+    i<-i+1
+  }
+  
+  if (!test)
+    stop("performanceTable ranges do not lie in valueFunctions ranges")
+  
   # determine first how many breakpoints in each value function
-
+  
   criteriaNumberOfBreakPoints <- c()
-
+  
   for (i in 1:numCrit)
     criteriaNumberOfBreakPoints <- c(criteriaNumberOfBreakPoints, dim(valueFunctions[[i]])[2])
-
+  
   criteriaBreakPoints <- list()
-	
-	for (i in 1:numCrit){
-   tmp <- valueFunctions[[i]]["x",]
-	 criteriaBreakPoints <- c(criteriaBreakPoints,list(tmp))
-	}
-	
-	names(criteriaBreakPoints) <- colnames(performanceTable)
-	
-	# -------------------------------------------------------
-	
-	# a is a matrix decomposing the alternatives in the break point space
-	
-	a<-matrix(0,nrow=numAlt, ncol=(sum(criteriaNumberOfBreakPoints)))
-	
-	for (n in 1:numAlt){
-	  for (m in 1:numCrit){
-	    if (length(which(performanceTable[n,m]==criteriaBreakPoints[[m]]))!=0){
-	      # then we have a performance value which is on a breakpoint
-	      j<-which(performanceTable[n,m]==criteriaBreakPoints[[m]])
-	      if (m==1)
-	        pos <- j
-	      else
-	        pos<-sum(criteriaNumberOfBreakPoints[1:(m-1)])+j
-	      a[n,pos] <- 1
-	    }
-	    else{
-	      # then we have value which needs to be approximated by a linear interpolation
-	      # let us first search the lower and upper bounds of the interval of breakpoints around the value
-	      if (criteriaMinMax[m] == "min"){
-	        j<-which(performanceTable[n,m]>criteriaBreakPoints[[m]])[1]-1
-	      }
-	      else{
-	        j<-which(performanceTable[n,m]<criteriaBreakPoints[[m]])[1]-1			
-	      }
-	      if (m==1)
-	        pos <- j
-	      else
-	        pos<-sum(criteriaNumberOfBreakPoints[1:(m-1)])+j
-	      a[n,pos] <- 1-(performanceTable[n,m]-criteriaBreakPoints[[m]][j])/(criteriaBreakPoints[[m]][j+1] - criteriaBreakPoints[[m]][j])
-	      a[n,pos+1] <- (performanceTable[n,m]-criteriaBreakPoints[[m]][j])/(criteriaBreakPoints[[m]][j+1] - criteriaBreakPoints[[m]][j])
-	    }
-	  }
-	}
-
+  
+  for (i in 1:numCrit){
+    tmp <- valueFunctions[[i]]["x",]
+    criteriaBreakPoints <- c(criteriaBreakPoints,list(tmp))
+  }
+  
+  names(criteriaBreakPoints) <- colnames(performanceTable)
+  
+  # -------------------------------------------------------
+  
+  # a is a matrix decomposing the alternatives in the break point space
+  
+  a<-matrix(0,nrow=numAlt, ncol=(sum(criteriaNumberOfBreakPoints)))
+  
+  for (n in 1:numAlt){
+    for (m in 1:numCrit){
+      if (length(which(performanceTable[n,m]==criteriaBreakPoints[[m]]))!=0){
+        # then we have a performance value which is on a breakpoint
+        j<-which(performanceTable[n,m]==criteriaBreakPoints[[m]])
+        if (m==1)
+          pos <- j
+        else
+          pos<-sum(criteriaNumberOfBreakPoints[1:(m-1)])+j
+        a[n,pos] <- 1
+      }
+      else{
+        # then we have value which needs to be approximated by a linear interpolation
+        # let us first search the lower and upper bounds of the interval of breakpoints around the value
+        if (criteriaMinMax[m] == "min"){
+          j<-which(performanceTable[n,m]>criteriaBreakPoints[[m]])[1]-1
+        }
+        else{
+          j<-which(performanceTable[n,m]<criteriaBreakPoints[[m]])[1]-1			
+        }
+        if (m==1)
+          pos <- j
+        else
+          pos<-sum(criteriaNumberOfBreakPoints[1:(m-1)])+j
+        a[n,pos] <- 1-(performanceTable[n,m]-criteriaBreakPoints[[m]][j])/(criteriaBreakPoints[[m]][j+1] - criteriaBreakPoints[[m]][j])
+        a[n,pos+1] <- (performanceTable[n,m]-criteriaBreakPoints[[m]][j])/(criteriaBreakPoints[[m]][j+1] - criteriaBreakPoints[[m]][j])
+      }
+    }
+  }
+  
   # -------------------------------------------------------
   
   normalizedPerformanceTable <- matrix(nrow=numAlt, ncol=numCrit)
-
+  
   for (m in 1:numCrit){
     if (m==1){
       start = 1
@@ -143,8 +159,8 @@ applyPiecewiseLinearValueFunctionsOnPerformanceTable <- function(valueFunctions,
     normalizedPerformanceTable[,m] <- a[,start:end]%*%valueFunctions[[m]]["y",]
   }
   rownames(normalizedPerformanceTable) <- rownames(performanceTable)
-
+  
   colnames(normalizedPerformanceTable) <- colnames(performanceTable)
-
+  
   return(normalizedPerformanceTable)
 }
