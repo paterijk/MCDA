7da1a234212f7f9231b05781f779e72220ac5f9d
Patrick Meyer <paterijk@gmail.com>
2016-10-26 13:40:30 +0200

    Added examples from article on the MCDA package.

--- tests/example2.R
+++ tests/example2.R
@@ -3,9 +3,14 @@ library(MCDA)
 # load performance table csv file
 # provided with the MCDA package
 
-f <- system.file("datasets","performanceTable.csv",package="MCDA")
+f <- system.file("datasets","performanceTable2.csv",package="MCDA")
 
 pT <- read.csv(file = f, header=TRUE, row.names=1)
+
+###################
+# first attempt   #
+# filtering rules #
+###################
 
 # to filter out cars which do not
 # respect Thierry's initial rules
@@ -24,6 +29,11 @@ names(criteriaMinMax) <- colnames(pT)
 
 plotRadarPerformanceTable(fPT, criteriaMinMax, overlay=FALSE, bw=TRUE, lwd =5)
 
+##################################
+# second attempt                 #
+# normalization and weighted sum #
+##################################
+
 # 5 barplots 
 par(mfrow=c(2,3))
 for (i in 1:dim(pT)[2]){
@@ -35,6 +45,46 @@ for (i in 1:dim(pT)[2]){
   name <-paste(colnames(pT)[i]," (",criteriaMinMax[i],")", sep="")
   barplot(oPT[,i], main=name, names.arg = rownames(oPT), density = i*10, ylim = yaxis, xpd=FALSE)  
 }
+
+# normalization of the data from the performance table
+
+normalizationTypes <- c("percentageOfMax","percentageOfMax",
+                        "percentageOfMax","percentageOfMax", "percentageOfMax")
+
+names(normalizationTypes) <- c("g1","g2","g3","g4","g5")
+
+nPT <- normalizePerformanceTable(pT,normalizationTypes)
+
+# weighted sum
+
+w <- c(-1,-2,-1,0.5,0.5)
+names(w) <- colnames(pT)
+ws<-weightedSum(nPT,w)
+
+# rank the scores of the alternatives
+rank(-ws)
+
+# add supplementary car to pT
+
+missing <- c(16966,30,37.7,2.33,3.25)
+pT<-rbind(pT,missing)
+rownames(pT)[14] <- "a10"
+
+# normalization
+
+nPT <- normalizePerformanceTable(pT,normalizationTypes)
+
+# weighted sum
+
+ws<-weightedSum(nPT,w)
+
+# rank the scores of the alternatives
+rank(-ws)
+
+###################
+# third attempt   #
+# MAVT            #
+###################
 
 # ranks of the alternatives
 
@@ -75,9 +125,9 @@ alternativesRanks <- c(1,2,3,4,5,6,7)
 names(alternativesRanks) <- c("a11","a03","a08","a04","a13","a09","a14")
 
 x2<-UTA(pT, criteriaMinMax, 
-       criteriaNumberOfBreakPoints, epsilon, 
-       alternativesRanks = alternativesRanks,
-       criteriaLBs = criteriaLBs, criteriaUBs = criteriaUBs)
+        criteriaNumberOfBreakPoints, epsilon, 
+        alternativesRanks = alternativesRanks,
+        criteriaLBs = criteriaLBs, criteriaUBs = criteriaUBs)
 
 # plot the piecewise linear value functions
 
@@ -89,30 +139,8 @@ tPT <- applyPiecewiseLinearValueFunctionsOnPerformanceTable(x2$valueFunctions, p
 
 # calculate the overall score of each alternative
 
-ws <- weightedSum(tPT,rep(1,5))
+mavt <- weightedSum(tPT,rep(1,5))
 
 # plot the ranking of the cars
 
-plotAlternativesValuesPreorder(ws, decreasing=TRUE)
-
-# normalization of the data from the performance table
-
-normalizationTypes <- c("percentageOfMax","percentageOfMax",
-                        "percentageOfMax","percentageOfMax", "percentageOfMax")
-
-names(normalizationTypes) <- c("g1","g2","g3","g4","g5")
-
-nPT <- normalizePerformanceTable(pT,normalizationTypes)
-
-# weighted sum
-
-w <- c(-1,-2,-1,0.5,0.5)
-names(w) <- colnames(pT)
-ws2<-weightedSum(nPT,w)
-
-# rank the scores of the alternatives
-one <- rank(-ws2)
-
-# Kendall's rank correlation index between ws2 and ws
-
-cor(rank(-ws2),rank(-ws), method="kendall")
+plotAlternativesValuesPreorder(mavt, decreasing=TRUE)
